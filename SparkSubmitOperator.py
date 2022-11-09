f0d8f2f1f08eeaaf0ea346a12d779007d1975a1e
amal209 <elhadyne.amal@gmail.com>
2022-11-09 16:19:30 +0100

    SparkOperator

--- SparkSubmitOperator.py
+++ SparkSubmitOperator.py
@@ -11,14 +11,17 @@ default_args = {
 with DAG(
     dag_id='spark_submit_operator',
     default_args=default_args,
-    start_date=datetime(2022, 9, 26),
+    start_date=datetime(2022, 11, 9),
 ) as dag:
     spark_submit_local = SparkSubmitOperator(
-		application ='/tmp/test.py' ,
-		conn_id= 'spark_default', 
-		task_id='spark_submit_task', 
-		dag=dag
-		)
+        		application ='/tmp/NTTData-1.0-SNAPSHOT.jar',
+		        conn_id= 'spark_default', 
+		        task_id='spark_submit_task', 
+                application_args=["org.data_training.App","Customers","hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"],
+		        dag=dag
+    )
+
+	
 
 spark_submit_local
     
