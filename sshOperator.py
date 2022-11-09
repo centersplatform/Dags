16e7bac76aadda7d7ced2817990ce0e30ab08ebf
amal209 <elhadyne.amal@gmail.com>
2022-11-09 17:52:21 +0100

    SparkOperator

--- sshOperator.py
+++ sshOperator.py
@@ -9,6 +9,7 @@ default_args = {
     'retries':2
 }
 bash_spark = """spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"""
+cmd2='echo $PATH'
 with DAG(
     dag_id='ssh_operator',
     default_args=default_args,
@@ -18,7 +19,7 @@ with DAG(
     ssh_local = SSHOperator(
 		        ssh_conn_id= 'ssh_default', 
 		        task_id='sshsubmit_task', 
-                command=bash_spark,
+                command=cmd2,
 		        dag=dag
     )
 
