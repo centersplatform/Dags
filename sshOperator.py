ceca8c9fdfdab2574eed27821defe0fdf7b78e54
amal209 <elhadyne.amal@gmail.com>
2022-11-09 17:48:46 +0100

    SparkOperator

--- sshOperator.py
+++ sshOperator.py
@@ -8,13 +8,7 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 default_args = {
     'retries':2
 }
-bash_spark = """
-        /spark/bin/spark-submit 
-        --master spark://spark-master-svc:7077 
-        --class org.data_training.App
-        NTTData-1.0-SNAPSHOT.jar
-        Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv
- """
+bash_spark = """spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"""
 with DAG(
     dag_id='ssh_operator',
     default_args=default_args,
