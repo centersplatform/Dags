8f5dbc261328804d3fa266899907bd9f2b99667a
amal209 <elhadyne.amal@gmail.com>
2022-11-09 17:54:25 +0100

    SparkOperator

--- sshOperator.py
+++ sshOperator.py
@@ -10,6 +10,7 @@ default_args = {
 }
 bash_spark = """spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"""
 cmd2='echo $PATH'
+cmd3='kubectl exec -it spark-master-0 -n spark -- bash'
 with DAG(
     dag_id='ssh_operator',
     default_args=default_args,
@@ -19,7 +20,7 @@ with DAG(
     ssh_local = SSHOperator(
 		        ssh_conn_id= 'ssh_default', 
 		        task_id='sshsubmit_task', 
-                command=cmd2,
+                command=cmd3,
 		        dag=dag
     )
 
