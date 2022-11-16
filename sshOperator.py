a858c64ac58b4beb5284c9a229e70d89a7617a42
Amal El Hadyne <aelhadyn@emeal.nttdata.com>
2022-11-16 17:18:37 +0100

    ssh

--- sshOperator.py
+++ sshOperator.py
@@ -8,11 +8,14 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 default_args = {
     'retries':2
 }
-bash_spark = """spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"""
+
 cmd2='echo $PATH'
 cmd3='kubectl get pods -n airflow'
-cmd4='kubectl exec -it spark-master-0 -n spark  -- spark-submit --master spark://spark-master-svc:7077 /tmp/test.py'
-submit_job='kubectl exec -it spark-master-0 -n spark -- bash && spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv'
+cmd4='kubectl exec -it spark-master-0 -n spark  -- '
+cmd5="""
+spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App \
+tmp/NTTData-1.0-SNAPSHOT.jar Customers hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv
+"""
 with DAG(
     dag_id='ssh_operator',
     default_args=default_args,
@@ -22,7 +25,7 @@ with DAG(
     ssh_local = SSHOperator(
 		        ssh_conn_id= 'ssh_default', 
 		        task_id='ssh_submit_task', 
-                command=cmd4,
+                command=cmd4+cmd5,
 		        dag=dag
     )
 
