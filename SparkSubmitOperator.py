de1c145dda847ce73904ac8765c1a927d86406b8
amal209 <elhadyne.amal@gmail.com>
2022-12-16 16:14:46 +0100

    SparkOperator

--- SparkSubmitOperator.py
+++ SparkSubmitOperator.py
@@ -6,13 +6,13 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 
 default_args = {
     'retries':2,
-    'owner': 'spark',
+    'owner': 'hive',
 }
 
 with DAG(
     dag_id='spark_submit_operator',
     default_args=default_args,
-    start_date=datetime(2022, 11, 16),
+    start_date=datetime(2022, 12, 16),
 
 ) as dag:
     spark_submit_local = SparkSubmitOperator(
@@ -20,12 +20,11 @@ with DAG(
                 java_class = "org.data_training.App",
 		        conn_id= 'spark_default', 
 		        task_id='spark_submit_task', 
-                application_args=["Customers","hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"],
+                application_args=["LoadDataToDW"],
 		        dag=dag
     )
 
 
 spark_submit_local
 
-if __name__ == "__main__":
-    dag.cli()
+
