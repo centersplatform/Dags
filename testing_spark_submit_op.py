c0c4e7c53c3b5f621ea1f66bf258fa6ca5ece146
hichamkabel <hichamkabel@gmail.com>
2022-12-19 16:16:44 +0100

    testing sparksubmitoperator

--- testing_spark_submit_op.py
+++ testing_spark_submit_op.py
@@ -1,36 +1,31 @@
-from airflow import DAG
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
 
-# Define default_args dictionary to specify default parameters of the DAG
+
 default_args = {
+    'retries':2,
     'owner': 'hive',
-    #'depends_on_past': False
-    'start_date': datetime(2022, 12, 19),
 }
 
-# Instantiate a DAG with the default_args dictionary
-dag = DAG(
-    'spark_on_kubernetes_example',
+with DAG(
+    dag_id='spark_submit_operator',
     default_args=default_args,
-    #schedule_interval=timedelta(minutes=30)
-)
+    start_date=datetime(2022, 12, 19),
 
-# Define a SparkSubmitOperator task
-task = SparkSubmitOperator(
-    conn_id='spark_default',
-    task_id='spark_job',
-    dag=dag,
-    application='/opt/bitnami/spark/tmp/NTTData-1.0-SNAPSHOT.jar',
-    java_class = "org.data_training.App",
-    application_args=["LoadDataToDW"],
-    in_cluster=True,
-    kubernetes_namespace='spark',
-    executor_memory='8G',
-    executor_cores='4'
-)
+) as dag:
+    spark_submit_local = SparkSubmitOperator(
+        		application ='/opt/bitnami/spark/tmp/NTTData-1.0-SNAPSHOT.jar',
+                #application = '/tmp/test.py',
+                java_class = "org.data_training.App",
+		        conn_id= 'spark_default', 
+		        task_id='spark_submit_task', 
+                application_args=["LoadDataToDW"],#,"hdfs://192.168.182.17:8020/hive/warehouse/hive/warehouse/ecom.db/customers_dataset/customers_dataset.csv"],
+		        dag=dag,
+                executor_memory='8G',
+                executor_cores='4'
+    )
 
-task
 
-# Set the dependencies for the task
-
+spark_submit_local
